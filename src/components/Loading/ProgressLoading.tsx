73495418b99d2c8ff7994106fc33961160966de0
NivedGanga <nivedganga@gmail.com>
2025-06-20 12:40:28 +0530

    fix: vercel config added

--- src/components/Loading/ProgressLoading.tsx
+++ src/components/Loading/ProgressLoading.tsx
@@ -5,7 +5,7 @@ function ProgressLoading(props: Props) {
     const { } = props
 
     return (
-        <div className='w-full z-10 grid place-items-center fixed top-0 left-0 h-full bg-[#0000005f] backdrop-blur-xs text-5xl text-white'>
+        <div className='w-full z-10 grid place-items-center fixed top-0 left-0 h-full bg-[#0000005f] backdrop-blur-md text-5xl text-white'>
             <img src={loadingAnimation} alt="" />
         </div>
     )
